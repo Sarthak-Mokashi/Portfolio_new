d5ccd2ec2a69c7f0b09b279669b36fc31fbed31d
Sarthak-Mokashi <sarthakmokashi41@gmail.com>
2021-03-21 21:56:56 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IntroComponent } from './intro/intro.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HeaderComponent } from './header/header.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ContentComponent } from './content/content.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { ClientsComponent } from './clients/clients.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { SocialmediaComponent } from './socialmedia/socialmedia.component';
+import { AboutComponent } from './about/about.component';
+import { BlogsComponent } from './blogs/blogs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroComponent,
+    NavigationComponent,
+    HeaderComponent,
+    GalleryComponent,
+    ContentComponent,
+    TestimonialsComponent,
+    ClientsComponent,
+    PricingComponent,
+    FooterComponent,
+    HomeComponent,
+    SocialmediaComponent,
+    AboutComponent,
+    BlogsComponent
   ],
   imports: [
     BrowserModule,
